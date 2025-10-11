09d687b60df70ac7111a6b494bf74530d789c0c6
LaHechicera <ca.gonzalezz@duocuc.cl>
2025-10-11 09:32:20 -0300

    Commit prueba

--- app/src/main/java/com/example/petdateapp/MainActivity.kt
+++ app/src/main/java/com/example/petdateapp/MainActivity.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import com.example.petdateapp.ui.theme.PetDateAPPTheme
 
 class MainActivity : ComponentActivity() {
