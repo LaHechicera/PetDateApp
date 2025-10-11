ddd28a308c7f250cee25b2c25dcd891c54f32024
LaHechicera <ca.gonzalezz@duocuc.cl>
2025-10-11 09:31:03 -0300

    Primer commit con mi Commit prueba

--- app/src/main/java/com/example/petdateapp/MainActivity.kt
+++ app/src/main/java/com/example/petdateapp/MainActivity.kt
@@ -9,8 +9,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.petdateapp.ui.theme.PetDateAPPTheme
 
 class MainActivity : ComponentActivity() {
